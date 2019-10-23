9c3aeb341d0b806c472627fd40b03a3f00ec5c73
Eric Lee Santos <eric_lee@MacBook-Pro-de-Eric.local>
2019-10-23 19:45:50 -0300

    atualizando desafio2.cpp

--- app_desafio3/desafio2_eric.cpp
+++ app_desafio3/desafio2_eric.cpp
@@ -11,8 +11,7 @@ using namespace std;
 
 int main()
 {
-    system("clear");
-    // Inicializando comunicacao e funcoes______________________
+    // Inicializando comunicacao e funcoes
     unsigned short handle;
     short ret;
   	char ip_maq[]="10.202.32.12";
@@ -20,52 +19,43 @@ int main()
     ret = cnc_startupprocess(0,"test.log");
     ret = cnc_allclibhndl3(ip_maq, 8193, 10, &handle);
 
-    // Extraindo dados de mensagem do PMC________________________
-    OPMSG3 opmsg;
-  	char pmc_msg[200];
-  	short int read_num=5;
-  	cnc_rdopmsg3(handle,0,&read_num,&opmsg);
-  	if (opmsg.datano==-1)
-  	{
-  		strcpy(pmc_msg,"NO ALARM\0");
-  	}
-  	else
-  	{
-  		strcpy(pmc_msg,opmsg.data);
-  	}
+    if (!ret)
+    {
+      // Extraindo dados de mensagem do PMC
+      OPMSG3 opmsg;
+      char pmc_msg[200];
+      short int read_num=5;
+      cnc_rdopmsg3(handle,0,&read_num,&opmsg);
+      if (opmsg.datano==-1)
+      {
+        strcpy(pmc_msg,"NO ALARM\0");
+      }
+      else
+      {
+        strcpy(pmc_msg,opmsg.data);
+      }
 
-    // Extraindo dados de status da maquina_______________________
-    ODBST buf;
-  	cnc_statinfo(handle, &buf);
-    int alm_info = buf.alarm;
-    int edit_info = buf.edit;
-    int aut_info = buf.aut;
-    int emg_info = buf.emergency;
-    int run_info = buf.motion;
-    int motion_info = buf.run;
+      // Extraindo dados de status da maquina
+      ODBST buf;
+      cnc_statinfo(handle, &buf);
 
-    // Criando pacote de dados a ser enviado_____________________
-    char package[500];
-    sprintf(package, "PMC Message: %s\nAlarm status: %d\nEmg status: %d\nRun status: %d\nMotion status: %d\nThis is ERIC LEE\n",pmc_msg,alm_info,emg_info,run_info,motion_info);
+      // Criando pacote de dados a ser enviado
+      char package[500];
+      sprintf(package, "PMC Message: %s\nAlarm status: %d\nEmg status: %d\nRun status: %d\nMotion status: %d\nThis is ERIC LEE\n",pmc_msg,buf.alarm,buf.emergency,buf.run,buf.motion);
 
-    // Enviando os dados para rede_______________________________
-    cout<<"\n====================== Server Output =========================\n";
-    HTTP servidor;
-    servidor.sendData("52.67.164.23","80", "/", "text/plain",package);
+      // Enviando os dados para rede
+      HTTP servidor;
+      servidor.sendData("18.191.146.49","80", "/", "application/json",package);
 
-    // Finalizando conexão_______________________________________
-    cnc_exitprocess();
-    cout<<"\n====================== Machine Output ========================\n";
-    cout<<"\n\nConection with machine status:\n"<<ret<<"\n";
-    cout<<pmc_msg<<"\n";
-    cout<<"\nGeneral machine status:\n";
-    cout<<"alm_info: "<<alm_info<<"\n";
-    cout<<"emg_info: "<<emg_info<<"\n";
-    cout<<"run_info: "<<run_info<<"\n";
-    cout<<"motion_info: "<<motion_info<<"\n";
-    cout<<"edit_info: "<<edit_info<<"\n";
-    cout<<"aut_info: "<<aut_info<<"\n";
-    cout<<"\n\n"<<package;
+      // Finalizando conexão
+      cnc_exitprocess();
+     
+      cout<<"\n"<<package<<"\n";
+    }
+    else
+    {
+      cout<<"machine is unreachable"
+    }
 
   	return 0;
 }

3e1c673c55f2c2debfaf932be1c8989850749632
root <root@ip-172-31-19-20.us-east-2.compute.internal>
2019-10-30 23:26:01 +0000

    depois do auxilio do tavares

--- app_desafio3/backend/index.js
+++ app_desafio3/backend/index.js
@@ -8,6 +8,11 @@ api.set('port', process.env.PORT || 6000);
 
 api.use(express.json());
 
+api.use(function(req, res, next){ 
+	console.log(req.path); 
+	next();
+});
+
 routes.build(api);
 
 api.listen(api.get('port'));
